14e1269300effabbb272a4d69b682dc007c85d9a
JUliano Luiz da Silva <juliano.silva@wisereducacao.com>
2020-12-27 02:09:52 -0300

    fix: rename title

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function  Home({ contents }) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Juliano Silva</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
